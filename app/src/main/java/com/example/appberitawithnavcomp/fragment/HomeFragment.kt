773727622d6d7d808d2eb031f83d332fb35549dd
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-21 11:19:30 +0700

    add model and viewModel

--- app/src/main/java/com/example/appberitawithnavcomp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/appberitawithnavcomp/fragment/HomeFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.appberitawithnavcomp.R
+import com.example.appberitawithnavcomp.databinding.FragmentHomeBinding
+import com.example.appberitawithnavcomp.databinding.ItemBeritaBinding
+import com.example.appberitawithnavcomp.viewmodel.BeritaViewModel
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var beritaViewModel: BeritaViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
